616653650631701059cd71f19774845cff2773bf
AILHC <505126057@qq.com>
2024-11-21 18:20:17 +0800

    [fix]修复edit-box中label的位置同步计算bug

--- cocos/ui/editbox/edit-box.ts
+++ cocos/ui/editbox/edit-box.ts
@@ -748,16 +748,24 @@ export class EditBox extends Component {
         node?.off(Sprite.EventType.SPRITE_FRAME_CHANGED, this._onBackgroundSpriteFrameChanged, this);
     }
 
-    protected _updateLabelPosition (size: Size): void {
+    protected _updateLabelPosition(size: Size): void {
         const trans = this.node._uiProps.uiTransformComp!;
-        const offX = -trans.anchorX * trans.width;
-        const offY = -trans.anchorY * trans.height;
-
+        const labelCenterX = (0.5 - trans.anchorX) * trans.width + LEFT_PADDING;
+        const labelCenterY = (0.5 - trans.anchorY) * trans.height;
+        // 默认两个label的锚点都是居中的
+        const labelContentWidth = size.width - LEFT_PADDING;
+        const labelContentHeight = size.height
         const placeholderLabel = this._placeholderLabel;
         const textLabel = this._textLabel;
         if (textLabel) {
-            textLabel.node._uiProps.uiTransformComp!.setContentSize(size.width - LEFT_PADDING, size.height);
-            textLabel.node.setPosition(offX + LEFT_PADDING, offY + size.height, textLabel.node.position.z);
+            const labelTrans = textLabel.node._uiProps.uiTransformComp;
+            labelTrans!.setContentSize(labelContentWidth, labelContentHeight);
+            // 已知锚点，算出中间位置
+            // labelTrans?.setAnchorPoint(0.5, 0.5);
+            // const offX = centerX - (0.5 - textLabelTrans!.anchorX) * textLabelTrans!.width;
+            // const offY = centerY - (0.5 - textLabelTrans!.anchorY) * textLabelTrans!.height;
+
+            textLabel.node.setPosition(labelCenterX, labelCenterY, textLabel.node.position.z);
             if (this._inputMode === InputMode.ANY) {
                 textLabel.verticalAlign = VerticalTextAlignment.TOP;
             }
@@ -765,8 +773,10 @@ export class EditBox extends Component {
         }
 
         if (placeholderLabel) {
-            placeholderLabel.node._uiProps.uiTransformComp!.setContentSize(size.width - LEFT_PADDING, size.height);
-            placeholderLabel.node.setPosition(offX + LEFT_PADDING, offY + size.height, placeholderLabel.node.position.z);
+            const labelTrans = placeholderLabel.node._uiProps.uiTransformComp;
+            // labelTrans?.setAnchorPoint(0.5, 0.5);
+            labelTrans!.setContentSize(labelContentWidth, labelContentHeight);
+            placeholderLabel.node.setPosition(labelCenterX, labelCenterY, placeholderLabel.node.position.z);
             placeholderLabel.enableWrapText = this._inputMode === InputMode.ANY;
         }
     }
